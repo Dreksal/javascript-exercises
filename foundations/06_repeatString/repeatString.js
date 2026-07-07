dc9ce49ded9d053b8ccc3df9c630ac166fb5a243
DaroMac <darek98259@gmail.com>
2026-07-07 21:02:06 +0200

    zrobione zadanko 06-07 z foundations

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (num < 0) {
+    return "ERROR";
+  }
+  let strToReturn = "";
+  for (let i = 0; i < num; i++) {
+    strToReturn += string;
+  }
+  return strToReturn;
 };
 
 // Do not edit below this line
